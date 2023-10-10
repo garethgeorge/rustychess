5d8e99dc5615de6f2b2caffdd96617d388ac3937
garethgeorge <garethgeorge97@gmail.com>
2023-10-09 17:20:14 -0700

    fix points evaluator

--- chessbot/src/evaluator/pointseval.rs
+++ chessbot/src/evaluator/pointseval.rs
@@ -10,23 +10,26 @@ impl PointsEval {
 
 impl Evaluator for PointsEval {
     fn evaluate(&self, board: &chess::Board) -> anyhow::Result<f32> {
-        let mut points = 0;
+        let mut points: i32 = 0;
         let white = board.color_combined(chess::Color::White);
         let black = board.color_combined(chess::Color::Black);
 
-        points += (board.pieces(chess::Piece::Pawn) & white).popcnt();
-        points -= (board.pieces(chess::Piece::Pawn) & black).popcnt();
-        points += (board.pieces(chess::Piece::Knight) & white).popcnt() * 3;
-        points -= (board.pieces(chess::Piece::Knight) & black).popcnt() * 3;
-        points += (board.pieces(chess::Piece::Bishop) & white).popcnt() * 3;
-        points -= (board.pieces(chess::Piece::Bishop) & black).popcnt() * 3;
-        points += (board.pieces(chess::Piece::Rook) & white).popcnt() * 5;
-        points -= (board.pieces(chess::Piece::Rook) & black).popcnt() * 5;
-        points += (board.pieces(chess::Piece::Queen) & white).popcnt() * 9;
-        points -= (board.pieces(chess::Piece::Queen) & black).popcnt() * 9;
-        points += (board.pieces(chess::Piece::King) & white).popcnt() * 100;
-        points -= (board.pieces(chess::Piece::King) & black).popcnt() * 100;
+        points += (board.pieces(chess::Piece::Pawn) & white).popcnt() as i32;
+        points -= (board.pieces(chess::Piece::Pawn) & black).popcnt() as i32;
+        points += (board.pieces(chess::Piece::Knight) & white).popcnt() as i32 * 3;
+        points -= (board.pieces(chess::Piece::Knight) & black).popcnt() as i32 * 3;
+        points += (board.pieces(chess::Piece::Bishop) & white).popcnt() as i32 * 3;
+        points -= (board.pieces(chess::Piece::Bishop) & black).popcnt() as i32 * 3;
+        points += (board.pieces(chess::Piece::Rook) & white).popcnt() as i32 * 5;
+        points -= (board.pieces(chess::Piece::Rook) & black).popcnt() as i32 * 5;
+        points += (board.pieces(chess::Piece::Queen) & white).popcnt() as i32 * 9;
+        points -= (board.pieces(chess::Piece::Queen) & black).popcnt() as i32 * 9;
+        points += (board.pieces(chess::Piece::King) & white).popcnt() as i32 * 100;
+        points -= (board.pieces(chess::Piece::King) & black).popcnt() as i32 * 100;
 
-        return Ok(points as f32);
+        return Ok(match board.side_to_move() {
+            chess::Color::White => points,
+            chess::Color::Black => -points,
+        } as f32);
     }
 }
